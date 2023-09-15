72a45cb6730728ed58e105209196883a34eef5e3
VuNguyen0901 <nguyencuongvu@gmail.com>
2023-09-15 14:51:54 +0700

    Update1

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,2 +1,15 @@
-package org.example;public class BankAccount {
+package org.example;
+
+public class BankAccount {
+
+    private int accountNumber;
+    private int accountBalance;
+    private String customerName;
+    private String email;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    private double phoneNumber;
 }
